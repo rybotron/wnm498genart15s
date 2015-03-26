e36b08884c1c114799b7631baab7da06462c0cf5
Ryan Berkey <iamryanberkey@gmail.com>
2015-03-26 16:11:15 -0700

    p5.js svg load

---
+++ week08/examples/class/SVG Load/sketch.js
@@ -0,0 +1,57 @@
+var img;
+function preload(){
+	img = loadImage('gradtwo.svg');
+}
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+  background(0);
+  image(img, 0, 0);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
